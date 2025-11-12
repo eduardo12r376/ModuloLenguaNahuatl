ef17f08972c2fbfc8a0245daacc034224612412d
Eduardo Hernández Rodríguez <80243476+eduardo12r376@users.noreply.github.com>
2025-11-12 13:31:37 -0600

    Update App.php

--- app/Config/App.php
+++ app/Config/App.php
@@ -17,7 +17,7 @@ class App extends BaseConfig
      * E.g., http://example.com/
      */
     //public string $baseURL = 'http://ModuloLenguaNahuatl.v/';
-    public string $baseURL = 'modulolenguanahuatl.netlify.app';
+    public string $baseURL = 'https://modulolenguanahuatl.netlify.app/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
